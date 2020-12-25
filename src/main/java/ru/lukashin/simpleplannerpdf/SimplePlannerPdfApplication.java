51a61b357855af2f4b1ec256901d8e172114f2ae
lukashindy <lukashindy@yandex.ru>
2020-12-26 00:07:34 +0300

    Initial commit

---
+++ src/main/java/ru/lukashin/simpleplannerpdf/SimplePlannerPdfApplication.java
@@ -0,0 +1,13 @@
+package ru.lukashin.simpleplannerpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePlannerPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePlannerPdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lukashin/simpleplannerpdf/SimplePlannerPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lukashin.simpleplannerpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePlannerPdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
